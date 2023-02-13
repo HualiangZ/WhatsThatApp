dd2f98d2f92cad4bcccf10dcf6e847ba18761c16
HualiangZ <zhualiang88@gmail.com>
2023-02-13 14:19:51 +0000

    Create project for app
    Create a new react native project

---
+++ WhatsThatAppM/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class WhatsThatApp extends Component {
+  render(){
+
+    return (
+        <View>
+          <Text>Hello World!</Text>
+        </View>
+    );
+  }
+}
+
+export default WhatsThatApp
