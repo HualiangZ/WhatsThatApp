fd5b4cb5ba41406459dbf1c684331387af77edc6
HualiangZ <zhualiang88@gmail.com>
2023-05-07 11:55:35 +0100

    login header and sign up head now shows

--- WhatsThatAppM/App.js
+++ WhatsThatAppM/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
+        <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Signup" component={SignupScreen} />
           <Stack.Screen name="Tab" component={Tab} />
